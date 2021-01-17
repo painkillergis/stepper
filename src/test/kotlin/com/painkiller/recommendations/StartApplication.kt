56c0b6f151fcaa0dc27aeaa19ce9e2dc4a0752a6
tyler <tyler@cruftbusters.com>
2021-01-16 18:25:08 -0600

    share application instance across each BSpec

--- src/test/kotlin/com/painkiller/recommendations/StartApplication.kt
+++ src/test/kotlin/com/painkiller/recommendations/StartApplication.kt
@@ -2,28 +2,29 @@ package com.painkiller.recommendations
 
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import org.junit.jupiter.api.extension.AfterAllCallback
 import org.junit.jupiter.api.extension.BeforeAllCallback
 import org.junit.jupiter.api.extension.ExtensionContext
 
-class StartApplication : BeforeAllCallback, AfterAllCallback {
-  lateinit var server: NettyApplicationEngine
+object StartApplication : BeforeAllCallback, ExtensionContext.Store.CloseableResource {
+  var server: NettyApplicationEngine? = null
 
   override fun beforeAll(context: ExtensionContext?) {
-    server = embeddedServer(
-      Netty,
-      applicationEngineEnvironment {
-        module {
-          applicationModule()
-        }
-        connector {
-          port = 8080
-        }
-      },
-    ).start()
+    if (server == null) {
+      server = embeddedServer(
+        Netty,
+        applicationEngineEnvironment {
+          module {
+            applicationModule()
+          }
+          connector {
+            port = 8080
+          }
+        },
+      ).start()
+    }
   }
 
-  override fun afterAll(context: ExtensionContext?) {
-    server.stop(1000, 10000)
+  override fun close() {
+    server!!.stop(1000, 10000)
   }
 }
