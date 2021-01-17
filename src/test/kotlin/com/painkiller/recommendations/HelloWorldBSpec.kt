1ce4755a2159ebd3094282a07cc2ced006491f94
tyler <tyler@cruftbusters.com>
2021-01-16 18:00:23 -0600

    split TestHttpClient from BSpec

--- src/test/kotlin/com/painkiller/recommendations/HelloWorldBSpec.kt
+++ src/test/kotlin/com/painkiller/recommendations/HelloWorldBSpec.kt
@@ -5,20 +5,24 @@ import io.ktor.client.request.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.api.extension.Extensions
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 @ExperimentalCoroutinesApi
-@ExtendWith(StartApplication::class)
+@Extensions(
+  ExtendWith(StartApplication::class),
+  ExtendWith(TestHttpClientProvider::class),
+)
 class HelloWorldBSpec {
+
+  @TestHttpClient
+  lateinit var httpClient : HttpClient
+
   @Test
   fun `get hello world returns greetings`() {
-    val client = HttpClient()
-
-    val bytes = runBlocking { client.get<ByteArray>("http://localhost:8080") }
+    val bytes = runBlocking { httpClient.get<ByteArray>("/") }
 
     assertEquals("Hello, world!", String(bytes))
-
-    client.close()
   }
 }
---
+++ src/test/kotlin/com/painkiller/recommendations/TestHttpClient.kt
@@ -0,0 +1,29 @@
+package com.painkiller.recommendations
+
+import io.ktor.client.*
+import io.ktor.client.features.*
+import io.ktor.client.request.*
+import org.junit.jupiter.api.extension.BeforeEachCallback
+import org.junit.jupiter.api.extension.ExtensionContext
+
+@Target(AnnotationTarget.FIELD)
+annotation class TestHttpClient
+
+object TestHttpClientProvider : BeforeEachCallback, ExtensionContext.Store.CloseableResource {
+  private val httpClient = HttpClient {
+    defaultRequest {
+      url("http://localhost:8080")
+    }
+  }
+
+  override fun beforeEach(context: ExtensionContext?) {
+    val instance = context!!.requiredTestInstance
+    instance.javaClass.declaredFields
+      .filter { it.isAnnotationPresent(TestHttpClient::class.java) }
+      .forEach { it.set(instance, httpClient) }
+  }
+
+  override fun close() {
+    httpClient.close()
+  }
+}
