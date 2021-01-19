4bd1fa655d079cd9f8beae2164148310676de38c
tyler <tyler@cruftbusters.com>
2021-01-18 23:26:23 -0600

    prepare Application.kt for extension

--- src/main/kotlin/com/painkiller/ktor_starter/Application.kt
+++ src/main/kotlin/com/painkiller/ktor_starter/Application.kt
@@ -3,15 +3,21 @@ package com.painkiller.ktor_starter
 import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.jackson.*
-import io.ktor.routing.*
 import io.ktor.server.netty.EngineMain.main
 
 fun main(args: Array<String>) = main(args)
 
 fun Application.applicationModule() {
-  routing {
-    VersionController().apply { routes() }
-  }
+  val controllers = listOf(
+    VersionController(
+      VersionService(),
+    ),
+  )
+  controllers.forEach { it.apply { module() } }
+  globalModules()
+}
+
+fun Application.globalModules() {
   install(ContentNegotiation) {
     jackson()
   }
