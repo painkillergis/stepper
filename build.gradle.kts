fffaedbe2f94484528d3750cb5580ced48e4e313
tyler <tyler@cruftbusters.com>
2021-01-20 17:32:30 -0600

    dependency locking

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,20 +22,21 @@ repositories {
 }
 
 dependencies {
-  implementation("io.ktor:ktor-html-builder:1.4.0")
-  implementation("io.ktor:ktor-jackson:1.4.0")
-  implementation("io.ktor:ktor-server-netty:1.4.0")
+  implementation("org.jetbrains.kotlin:kotlin-stdlib:+")
+  implementation("io.ktor:ktor-html-builder:+")
+  implementation("io.ktor:ktor-jackson:+")
+  implementation("io.ktor:ktor-server-netty:+")
   implementation("org.slf4j:slf4j-simple:+")
-  testImplementation("io.ktor:ktor-client-apache:1.4.0")
-  testImplementation("io.ktor:ktor-client-core:1.4.0")
-  testImplementation("io.ktor:ktor-client-jackson:1.4.0")
-  testImplementation("io.ktor:ktor-server-test-host:1.4.0")
+  testImplementation("io.ktor:ktor-client-apache:+")
+  testImplementation("io.ktor:ktor-client-core:+")
+  testImplementation("io.ktor:ktor-client-jackson:+")
+  testImplementation("io.ktor:ktor-server-test-host:+")
   testImplementation("io.mockk:mockk:+")
   testImplementation("org.hamcrest:hamcrest-junit:+")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:+")
-  testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+  testImplementation("org.junit.jupiter:junit-jupiter-api:+")
   testImplementation(kotlin("test-junit5"))
-  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:+")
 }
 
 tasks.test {
@@ -57,5 +58,29 @@ tasks.named("processResources") {
         val (sha, patch) = inputStream.bufferedReader().readText().split("\n")
         file("src/main/resources/version.properties").writeText("sha=$sha\nversion=v$major.$minor.$patch")
       }
+  }
+}
+
+configurations.all {
+  resolutionStrategy {
+    activateDependencyLocking()
+    componentSelection
+      .all(object : Action<ComponentSelection> {
+        @Mutate
+        override fun execute(selection: ComponentSelection) {
+          val version = selection.candidate.version
+          when {
+            version.matches(
+              Regex(
+                ".*-rc$",
+                RegexOption.IGNORE_CASE
+              )
+            ) -> selection.reject("Release candidates are excluded")
+            version.matches(Regex(".*-M\\d+$")) -> selection.reject("Milestones are excluded")
+            version.matches(Regex(".*-alpha\\d+$")) -> selection.reject("Alphas are excluded")
+            version.matches(Regex(".*-beta\\d+$")) -> selection.reject("Betas are excluded")
+          }
+        }
+      })
   }
 }
