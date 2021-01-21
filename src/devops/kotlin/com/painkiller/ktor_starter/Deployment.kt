415db7a61cf17ba03a117358bed1147ff1a45a2d
tyler <tyler@cruftbusters.com>
2021-01-21 01:42:30 -0600

    devops refactor

--- src/devops/kotlin/com/painkiller/ktor_starter/Deployment.kt
+++ src/devops/kotlin/com/painkiller/ktor_starter/Deployment.kt
@@ -4,16 +4,16 @@ import com.fkorotkov.kubernetes.apps.metadata
 import com.fkorotkov.kubernetes.apps.selector
 import com.fkorotkov.kubernetes.apps.spec
 
-class Deployment(name: String, podTemplateSpec: PodTemplateSpec) : io.fabric8.kubernetes.api.model.apps.Deployment() {
+class Deployment(deploymentName: String, podTemplateSpec: PodTemplateSpec) : io.fabric8.kubernetes.api.model.apps.Deployment() {
   init {
     metadata {
-      this.name = name
-      labels = mapOf("app" to name)
+      name = deploymentName
+      labels = mapOf("app" to deploymentName)
     }
     spec {
       replicas = 1
       selector {
-        matchLabels = mapOf("app" to name)
+        matchLabels = mapOf("app" to deploymentName)
       }
       template = podTemplateSpec
     }
--- src/devops/kotlin/com/painkiller/ktor_starter/Service.kt
+++ src/devops/kotlin/com/painkiller/ktor_starter/Service.kt
@@ -1,8 +1,9 @@
 package com.painkiller.ktor_starter
 
 import com.fkorotkov.kubernetes.metadata
+import com.fkorotkov.kubernetes.newServicePort
 import com.fkorotkov.kubernetes.spec
-import io.fabric8.kubernetes.api.model.ServicePortBuilder
+import io.fabric8.kubernetes.api.model.IntOrString
 
 class Service(val serviceName: String, val deploymentName: String) : io.fabric8.kubernetes.api.model.Service() {
   init {
@@ -12,10 +13,10 @@ class Service(val serviceName: String, val deploymentName: String) : io.fabric8.
     spec {
       type = "NodePort"
       ports = listOf(
-        ServicePortBuilder()
-          .withPort(8080)
-          .withNewTargetPort(8080)
-          .build()
+        newServicePort {
+          port = 8080
+          targetPort = IntOrString(8080)
+        }
       )
       selector = mapOf("app" to deploymentName)
     }
