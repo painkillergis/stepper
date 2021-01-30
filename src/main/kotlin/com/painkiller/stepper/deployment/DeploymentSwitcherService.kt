e7222aa47696ea07de4ecc16c70e28208951ea53
tyler <tyler@cruftbusters.com>
2021-01-29 19:20:24 -0600

    refactor deployment switcher service

--- src/main/kotlin/com/painkiller/stepper/deployment/DeploymentSwitcherService.kt
+++ src/main/kotlin/com/painkiller/stepper/deployment/DeploymentSwitcherService.kt
@@ -1,14 +1,16 @@
 package com.painkiller.stepper.deployment
 
+import io.fabric8.kubernetes.api.model.Service
 import io.fabric8.kubernetes.client.NamespacedKubernetesClient
+import io.fabric8.kubernetes.client.dsl.ServiceResource
 
 class DeploymentSwitcherService(val kubernetesClient: NamespacedKubernetesClient) {
   fun switchDeployments(firstServiceName: String, lastServiceName: String) {
     val firstService = kubernetesClient.services().withName(firstServiceName)
     val lastService = kubernetesClient.services().withName(lastServiceName)
 
-    val firstDeploymentName = firstService.get()?.spec?.selector?.get("app")
-    val lastDeploymentName = lastService.get()?.spec?.selector?.get("app")
+    val firstDeploymentName = deploymentName(firstService)
+    val lastDeploymentName = deploymentName(lastService)
 
     if (firstDeploymentName == null && lastDeploymentName == null) {
       throw Error("No deployments available to switch")
@@ -22,4 +24,7 @@ class DeploymentSwitcherService(val kubernetesClient: NamespacedKubernetesClient
       newPrefabService(lastServiceName, firstDeploymentName ?: "")
     )
   }
+
+  private fun deploymentName(serviceResource: ServiceResource<Service>) =
+    serviceResource.get()?.spec?.selector?.get("app")
 }
